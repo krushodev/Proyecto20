79a5a632d65c93220e8b4ba092ca1c1432d0e9a1
krushodev <ignakruchowski@gmail.com>
2026-04-10 16:18:06 -0300

    refactor: fix error sqllite, add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/catalogo', function () {
+    return view('catalogo');
+});
+
+Route::get('/comercializacion', function () {
+    return view('comercializacion');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/detalle-compra', function () {
+    return view('detalle-compra');
+});
+
+Route::get('/detalle-producto', function () {
+    return view('detalle-producto');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
+
+Route::get('/registro', function () {
+    return view('registro');
+});
+
+Route::get('/terminos-y-condiciones', function () {
+    return view('terminos-y-condiciones');
 });
